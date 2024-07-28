f341c0f696eee82627ba1511cdca43ca77415427
Daniel Janocha <daniel.jan.janocha@gmail.com>
2024-07-28 15:28:31 +0200

    fixed ypes in place of bluring search engine input

--- src/app/_components/event-search-by.tsx
+++ src/app/_components/event-search-by.tsx
@@ -3,12 +3,12 @@ import { useAtom } from "jotai";
 import { searchByAtom } from "~/atoms/search-by-atom";
 import { Input } from "~/components/ui/input";
 import {
-    Form,
-    FormControl,
-    FormDescription,
-    FormField,
-    FormItem,
-    FormMessage,
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormMessage,
 } from "~/components/ui/form";
 import { SubmitHandler, useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
@@ -17,45 +17,45 @@ import { z } from "zod";
 import { useCallback } from "react";
 type FormValues = z.infer<typeof filteredEventsInputSearchByValidator>;
 export const EventsSearchBy = () => {
-    const [searchBy, setSearchBy] = useAtom(searchByAtom);
-    const form = useForm<FormValues>({
-        resolver: zodResolver(filteredEventsInputSearchByValidator),
-        defaultValues: { ...searchBy },
-    });
-    const onSubmit = useCallback<SubmitHandler<FormValues>>((vals) => {
-        setSearchBy(vals);
-    }, []);
-    return (
-        <Form {...form}>
-            <form onChange={form.handleSubmit(onSubmit)} className="flex flex-1">
-                <div className="flex w-full flex-1 flex-col items-center gap-4">
-                    <FormField
-                        control={form.control}
-                        name={`searchBy`}
-                        render={({ field }) => (
-                            <FormItem className="w-full">
-                                <FormControl>
-                                    <Input
-                                        {...field}
-                                        placeholder="what event are you interested in?"
-                                        onKeyDown={(e) => {
-                                            console.log(e.key);
-                                            if (e.key === "Enter") {
-                                                e.preventDefault();
-                                                // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-                                                //@ts-ignore
-                                                document.activeElement.blur();
-                                            }
-                                        }}
-                                    />
-                                </FormControl>
-                                <FormDescription />
-                                <FormMessage />
-                            </FormItem>
-                        )}
-                    />
-                </div>
-            </form>
-        </Form>
-    );
+  const [searchBy, setSearchBy] = useAtom(searchByAtom);
+  const form = useForm<FormValues>({
+    resolver: zodResolver(filteredEventsInputSearchByValidator),
+    defaultValues: { ...searchBy },
+  });
+  const onSubmit = useCallback<SubmitHandler<FormValues>>((vals) => {
+    setSearchBy(vals);
+  }, []);
+  return (
+    <Form {...form}>
+      <form onChange={form.handleSubmit(onSubmit)} className="flex flex-1">
+        <div className="flex w-full flex-1 flex-col items-center gap-4">
+          <FormField
+            control={form.control}
+            name={`searchBy`}
+            render={({ field }) => (
+              <FormItem className="w-full">
+                <FormControl>
+                  <Input
+                    {...field}
+                    placeholder="what event are you interested in?"
+                    onKeyDown={(e) => {
+                      console.log(e.key);
+                      if (e.key === "Enter") {
+                        e.preventDefault();
+                        if (document.activeElement instanceof HTMLElement) {
+                          document.activeElement.blur();
+                        }
+                      }
+                    }}
+                  />
+                </FormControl>
+                <FormDescription />
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </div>
+      </form>
+    </Form>
+  );
 };
